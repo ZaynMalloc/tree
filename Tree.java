c11329818d369f5a0dc0350f276797cb4c3198a0
ZaynMalloc <zacharygacer@yahoo.com>
2016-11-21 16:01:46 -0500

    Creating tree class

---
+++ Tree.java
@@ -0,0 +1,16 @@
+public class Tree{
+  
+  private String data;
+  private Tree leftNode;
+  private Tree rightNode;
+  
+  public BinaryTree(int data) {
+    this.data = data;
+    this.leftNode = null;
+    this.rightNode = null;
+  }
+  
+  
+
+ 
+}
