1a7bc346245515389c5e9fa6a818a08e26e99286
ZaynMalloc <zacharygacer@yahoo.com>
2016-11-27 22:34:17 -0500

    Adding methods that add nodes to the tree

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+  
+  public static void main(String[] args){
+    
+   Node newNode = new Node("Hello World");
+   newNode.printNode();
+  }
+  
+}
---
+++ Node.java
@@ -0,0 +1,33 @@
+//Node Class
+ public class Node{
+
+  private String data;
+  private Node left;
+  private Node right;
+  
+  public Node(String data){
+    this.data = data;
+  }
+  
+  public void setLeft(Node myLeft){
+    this.left = myLeft; 
+  }
+  
+   public void setRight(Node myRight){
+     this.right = myRight; 
+  }
+   
+   public Node getLeft(){
+     return this.left;
+  }
+  
+   public Node getRight(Node right){
+     return this.right;
+  }
+   
+   public void printNode(){
+    System.out.println(this.data); 
+   }
+
+
+}
